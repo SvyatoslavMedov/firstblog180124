76e7934d14441b4a62b37a75a1585127d702dc46
SvyatoslavMedov <SvyatoslavMedov1204@gmail.com>
2024-01-18 13:43:06 +0600

    Initial commit

---
+++ src/main/java/com/medov/firstblog180124/Firstblog180124Application.java
@@ -0,0 +1,13 @@
+package com.medov.firstblog180124;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Firstblog180124Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Firstblog180124Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/medov/firstblog180124/Firstblog180124ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medov.firstblog180124;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Firstblog180124ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
